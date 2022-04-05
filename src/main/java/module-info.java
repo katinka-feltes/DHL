be19735e6ec1caee5a0590a5e81027041c541514
Philipp Bende <bende@isp.uni-luebeck.de>
2022-04-05 11:34:53 +0000

    Update module-info.java

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,9 @@
 /**
- * The main module of the chess application.
+ * The main module of the dhl application.
  */
-module chess {
+module dhl {
     requires javafx.controls;
     requires transitive javafx.graphics;
     
-    exports chess.gui;
+    exports dhl.gui;
 }
