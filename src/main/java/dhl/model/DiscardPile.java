7d9208762f0ce58b73eb7a113a44b2f5cc84d976
katinka-feltes <48929802+katinka-feltes@users.noreply.github.com>
2022-04-10 19:11:04 +0200

    added the class DiscardPile

---
+++ src/main/java/dhl/model/DiscardPile.java
@@ -0,0 +1,54 @@
+package dhl.model;
+
+import java.util.ArrayList;
+
+public class DiscardPile {
+    private ArrayList<Card> pile;
+    private char color;
+
+
+    /**
+     * The constructor for DiscardPile
+     * @param color the color that the pile will have
+     */
+    public DiscardPile(char color){
+        pile = new ArrayList<>();
+        this.color = color;
+    }
+
+    /**
+     * adds card to top (end) of the pile if the color is the same
+     * @param card the card that is added
+     */
+    public void addCard(Card card){
+        if (card == null || card.getColor()!=color){
+            System.out.println("Card is null or a different color than the pile (for your info carl");
+            return;
+        }
+        pile.add(card);
+    }
+
+    /**
+     * returns the top card (at the end of the list)
+     * @return the Card at the top of the Pile
+     */
+    public Card getTop(){
+        return pile.get(pile.size()-1);
+    }
+
+    /**
+     * returns and removes the top card (from the end of the list)
+     * @return the Card that was removed from the top of the Pile
+     */
+    public Card getAndRemoveTop(){
+        return pile.remove(pile.size()-1);
+    }
+
+    public char getColor() {
+        return color;
+    }
+
+    public ArrayList<Card> getPile (){
+        return pile;
+    }
+}
