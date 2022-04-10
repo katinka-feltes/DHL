315e4b4db76bd03e92b7c0292a884e52c4adcbf1
Marten <marten.buchmann@student.uni-luebeck.de>
2022-04-10 17:49:42 +0200

    Catching InputMismatchException when the player amount is no integer.

--- src/main/java/dhl/cli/Cli.java
+++ src/main/java/dhl/cli/Cli.java
@@ -1,8 +1,9 @@
 package dhl.cli;
 
 import dhl.model.Game;
-
 import java.util.Scanner;
+import java.util.InputMismatchException;
+
 
 /**
  * Starting point of the command line interface
@@ -15,13 +16,25 @@ public class Cli {
      */
     public static void main(String[] args) {
         System.out.println("How many players? (2, 3 or 4)");
-        Scanner scanner = new Scanner(System.in) ;
-        int playerAmount = scanner.nextInt(); //Error when input is no int
-        // check if the number is allowed
-        while (playerAmount < 2 || playerAmount > 4) {
-            System.out.println("Please enter a number between 2 and 4.");
-            playerAmount = scanner.nextInt();
+        Scanner scanner = new Scanner(System.in);
+        int playerAmount = 0; //initializing the variable playerAmount
+        boolean inputValid = false; //initializing the variable inputValid
+
+        while(!inputValid){ //checks if the input is an integer and if it is inbound
+            try{
+                playerAmount = scanner.nextInt();
+                // check if the number is inbound
+                while (playerAmount < 2 || playerAmount > 4) {
+                    System.out.println("Please enter a number between 2 and 4.");
+                    playerAmount = scanner.nextInt();
+                }
+                    inputValid = true;
+            } catch (InputMismatchException e){
+                System.out.println("Input must be an integer!");
+                scanner.nextLine();
+            }
         }
+
         Game game = new Game(playerAmount);
 
     }
