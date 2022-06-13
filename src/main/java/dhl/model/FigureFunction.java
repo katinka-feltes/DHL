eb9e6e03dfc3289e90db866b4b93fd6d305a52d2
cgathmann <c.gathmann@icloud.com>
2022-06-13 12:33:13 +0200

    removed unnecessary Fields in GuiController.

--- src/main/java/dhl/model/FigureFunction.java
+++ src/main/java/dhl/model/FigureFunction.java
@@ -12,9 +12,9 @@ public class FigureFunction {
      * @param fieldIndex the index of the field to get the figure-amount from
      * @return the amount of figures on the field as an int
      */
-    public static int getFigureAmountOnField(int fieldIndex, List<Figure> figures) {
+    public static int getFigureAmountOnField(int fieldIndex, List<dhl.model.Figure> figures) {
         int amount = 0;
-        for (Figure f : figures) {
+        for (dhl.model.Figure f : figures) {
             if (f.getPos() == fieldIndex) {
                 amount++;
             }
@@ -26,7 +26,7 @@ public class FigureFunction {
      * counts the figures in the finish area.
      * @return the amount of figures in the finish area.
      */
-    public static int getFigureAmountInFinishArea(List<Figure> figures){
+    public static int getFigureAmountInFinishArea(List<dhl.model.Figure> figures){
         int amount = 0;
         for(int field = 22; field <= 35; field++){ //should be 22 but for better testing 4
             amount += getFigureAmountOnField(field, figures);
@@ -38,8 +38,8 @@ public class FigureFunction {
      * @param figurePos 1 for the figure first on the board, 2 or 3
      * @return the figure on the field or null if there is none
      */
-    public static Figure getFigureByPos(int figurePos, List<Figure> figures) {
-        figures.sort(Comparator.comparing(Figure::getPos));
+    public static dhl.model.Figure getFigureByPos(int figurePos, List<dhl.model.Figure> figures) {
+        figures.sort(Comparator.comparing(dhl.model.Figure::getPos));
         return figures.get(figures.size() - figurePos);
     }
 
